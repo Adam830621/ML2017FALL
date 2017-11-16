740055b97b4754f6d3d0edde7e59048b9a25d4e2
Adam830621 <r05943136@ntu.edu.tw>
2017-11-17 02:57:11 +0800

    Add files via upload

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,1 +1,7 @@
+echo "Downloading model"
+
+wget --no-check-certificate 'https://www.dropbox.com/s/zp0bmrgyxss7faq/model_00027_0.68896.h5?dl=0' -O model_00027_0.68896.h5
+
+
+echo "run"
 python3 hw3_test.py $1 $2
