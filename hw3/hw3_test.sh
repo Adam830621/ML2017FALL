6884de91309c97c211436e9a689ec7109ba0f0e4
Adam830621 <r05943136@ntu.edu.tw>
2017-11-17 03:16:56 +0800

    Add files via upload

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,7 +1,7 @@
-echo "Downloading model"
+echo "---------------Downloading model---------------"
 
-wget --no-check-certificate 'https://www.dropbox.com/s/zp0bmrgyxss7faq/model_00027_0.68896.h5?dl=0' -O model_00027_0.68896.h5
-
-
-echo "run"
+wget --no-check-certificate 'https://www.dropbox.com/s/wkfr7dslre3ytk6/model_00027_0.68896.zip?dl=0' -O model_00027_0.68896.zip
+unzip model_00027_0.68896.zip
+rm model_00027_0.68896.zip
+echo "---------------Start testing---------------"
 python3 hw3_test.py $1 $2
