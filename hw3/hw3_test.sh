3ff2f17e635bf0af7286074f588421e8a3698ef1
Adam830621 <r05943136@ntu.edu.tw>
2017-11-17 03:35:56 +0800

    Delete hw3_test.sh

--- hw3/hw3_test.sh
+++
@@ -1,7 +0,0 @@
-echo "---------------Downloading model---------------"
-
-wget --no-check-certificate 'https://www.dropbox.com/s/wkfr7dslre3ytk6/model_00027_0.68896.zip?dl=0' -O model_00027_0.68896.zip
-unzip model_00027_0.68896.zip
-rm model_00027_0.68896.zip
-echo "---------------Start testing---------------"
-python3 hw3_test.py $1 $2
